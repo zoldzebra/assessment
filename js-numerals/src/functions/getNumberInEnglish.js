7b6de66c59a0a6b2e9a8f6a798bc31b5c41d8168
zoldzebra <zoldzebra@gmail.com>
2019-04-24 18:48:12 +0200

    Translate under hundred and init tests

--- js-numerals/src/functions/getNumberInEnglish.js
+++ js-numerals/src/functions/getNumberInEnglish.js
@@ -4,6 +4,22 @@
 // 1999 == nineteen hundred and ninety-nine
 // 17999 == seventeen thousand nine hundred and ninety-nine
 
-const getNumberInEnglish = number => number;
+const uniques = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelwe', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+const tens = [undefined, undefined, 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+const getNumberInEnglish = number => translateUnderHundred(number);
+
+const translateUnderHundred = (number) => {
+    number = parseInt(number);
+    let underHundred = '';
+    if (number < 20) {
+        underHundred += uniques[number];
+    } else if (number % 10 === 0) {
+        underHundred += tens[number / 10];
+    } else if (number < 100) {
+        underHundred += tens[(number - number % 10) / 10] + '-' + uniques[number % 10];
+    }
+    return underHundred;
+}
 
 module.exports = getNumberInEnglish;
---
+++ js-numerals/test/test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const chai = require('chai');
+const expect = require('chai').expect;
+const getNumberInEnglish = require('../src/functions/getNumberInEnglish');
+// 7    == seven
+// 42   == forty-two
+// 2001 == two thousand and one
+// 1999 == nineteen hundred and ninety-nine
+// 17999 == seventeen thousand nine hundred and ninety-nine
+
+describe('JS numerals tests', () => {
+    it('should translate under 10', () => {
+        expect(getNumberInEnglish('7')).to.equal('seven');
+    });
+    it('should translate under 20', () => {
+        expect(getNumberInEnglish('16')).to.equal('sixteen');
+    });
+    it('should translate under 100', () => {
+        expect(getNumberInEnglish('42')).to.equal('forty-two');
+    });
+    it('should discard zeros in the beginning of number', () => {
+        expect(getNumberInEnglish('00013')).to.equal('thirteen');
+    });
+});
